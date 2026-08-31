cd44f00b87dd0db474db74c06ec93ef4ce9f90c8
MM-rrrr <yuunoza0728@gmail.com>
2026-08-31 14:26:31 +0900

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+const button = document.querySelector('.menu-btn');
+const nav = document.querySelector('.nav');
+if (button && nav) {
+  button.addEventListener('click', () => {
+    const open = nav.classList.toggle('open');
+    button.setAttribute('aria-expanded', String(open));
+  });
+}
